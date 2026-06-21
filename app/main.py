1fcb5f2fb6d192bed5157ee1a6d9998b4eaac514
Phuc1912001 <phucphuc1912001@gmail.com>
2026-06-21 21:08:34 +0700

    fix cors, add env link FE

--- app/main.py
+++ app/main.py
@@ -17,6 +17,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://chuanword.vercel.app/",
+        "https://www.chuanword.vn/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
