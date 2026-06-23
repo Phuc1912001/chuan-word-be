d416a61e5f59a292a806965c7c6ab4394a2a855d
Phuc1912001 <phucphuc1912001@gmail.com>
2026-06-23 21:25:22 +0700

    add logic count

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,10 +2,12 @@ from app.db.base import Base
 from app.models.file import UploadedFile
 from app.models.job import AnalysisJob
 from app.models.payment import Payment
+from app.models.statistics import Statistic
 
 __all__ = [
     "Base",
     "UploadedFile",
     "AnalysisJob",
     "Payment",
+    "Statistic",
 ]
